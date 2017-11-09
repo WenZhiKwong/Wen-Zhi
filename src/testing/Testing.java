bd8890a9e2fd5fceaf172fdf875a1d8b2ef5f4a9
Student <Student@PC80>
2017-11-09 14:47:16 +0800

    [Delivers #152707854]

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -18,6 +18,7 @@ public class Testing {
         // TODO code application logic here
         System.out.println("Hello");
         System.out.println("World");
+        System.out.println("AIiiiii");
     }
     
 }
